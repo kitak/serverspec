195552688a2ae8d079e6faae93e587f45176fa9e
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-04 17:31:55 +0900

    use `inspect` for opts

--- lib/serverspec/type/base.rb
+++ lib/serverspec/type/base.rb
@@ -11,7 +11,7 @@ module Serverspec
         type.gsub!(/([a-z\d])([A-Z])/, '\1 \2')
         type.capitalize!
         if @opts.size > 0
-          %Q!#{type} "#{@name}, #{@opts.join(", ")}"!
+          %Q!#{type} "#{@name}, #{@opts.map(&:inspect).join(", ")}"!
         else
           %Q!#{type} "#{@name}"!
         end
